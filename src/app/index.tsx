7bfcf9b59127183c00a2fd3af76679b241b33db2
Florian Gesell <fgesell@hotmail.de>
2022-07-12 14:48:36 +0200

    added clean folder structure

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../assets/images/logo.svg';
+import '../assets/style/index.css';
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Now
         </a>
       </header>
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './assets/style/index.css';
+import App from './app';
+import reportWebVitals from './features/reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement

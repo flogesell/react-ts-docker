737de6dae87e599014375545db2e19693248739c
Florian Gesell <f.gesell@uniqbit.de>
2022-07-12 15:49:41 +0200

    Tuto 4

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,35 @@
+import { isPropertySignature } from 'typescript';
 import '../assets/style/index.css';
+import { Greet } from '../components/Greet';
+import { Person } from '../components/Person';
+import { PersonList } from '../components/personList';
 
 function App() {
+  const personName = {
+    first: 'Bruce',
+    last: 'Wayne'
+  }
+
+  const nameList = [
+    {
+      first: 'Bruce',
+      last: 'Wayne'
+    },
+    {
+      first: 'Clark',
+      last: 'Kent'
+    },
+    {
+      first: 'Princess',
+      last: 'Diana'
+    },
+  ]
+
   return (
     <div className="App">
+      <Greet name="Flo" messageCount={20} isLoggedIn={true} />
+      <Person name={ personName }/>
+      <PersonList names={nameList}/>
     </div>
   );
 }
---
+++ src/components/Person.tsx
@@ -0,0 +1,10 @@
+type personProps = {
+    name: {
+        first: string
+        last: string
+    }
+}
+
+export const Person = (props: personProps) => {
+    return <div>{props.name.first} {props.name.last}</div>
+}
